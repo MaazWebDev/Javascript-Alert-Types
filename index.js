e0b9288bed9f1c1e565c1a68305e39a79858c72d
MaazWebDev <maazkhan66376@gmail.com>
2024-03-05 14:36:22 +0500

    Make the alerts on he browser
    Using javascript make the alerts carring different messages display on browser

---
+++ index.js
@@ -0,0 +1,5 @@
+alert("Hey! How are you ?");
+alert("Error! Please enter a valid Password.");
+alert("Welcome to JS land\nHappy Coding !");
+alert("Happy Coding!\nLets Try...");
+alert("Hello.. I can run JS through my web browser's console.");
